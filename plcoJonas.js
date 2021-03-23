3768fdfeeeff491ff52bdcf84e6afef52fcae12d
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2021-03-22 20:58:04 -0400

    Update plcoJonas.js

--- plcoJonas.js
+++ plcoJonas.js
@@ -6,7 +6,6 @@ if(typeof(plco)=='undefined'){
 
 if(!plco.api){plco.api={}}
 
-//plco.api.url='https://exploregwas-dev.cancer.gov/plco-atlas/api/'
 plco.api.url='https://api.box.com/2.0/files/790147208541/content?version=4'
 
 plco.api.ping=async()=>{
