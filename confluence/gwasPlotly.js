926b71df3e198cd566376c1801272525d5c43933
Jonas Almeida <jalmeida@mathbiol.org>
2021-04-14 23:25:24 -0400

    confluence chromossomes

---
+++ confluence/gwasPlotly.js
@@ -0,0 +1,79 @@
+console.log('gwasPlotly.js loaded')
+
+convert2Plotly=async(url="GWAS_data1000.json")=>{
+    let dt = await(await fetch(url)).json()
+    switch(url) {
+      case "GWAS_data1000.json":
+        // code block
+        console.log(dt)
+        convert2Plotly.gwas(dt)
+        break;
+
+      case "GWAS_data374674.json":
+        console.log(dt)
+        convert2Plotly.gwas(dt)
+        break;
+      
+      default:
+        console.warn(`data URL not found: ${url}`)
+    }
+}
+
+convert2Plotly.saveFile=(x,fileName)=>{ // x is the content of the file
+	// var bb = new Blob([x], {type: 'application/octet-binary'});
+	// see also https://github.com/eligrey/FileSaver.js
+	let bb = new Blob([x]);
+   	let url = URL.createObjectURL(bb);
+	let a = document.createElement('a');
+   	a.href=url;
+	if (fileName){
+		if(typeof(fileName)=="string"){ // otherwise this is just a boolean toggle or something of the sort
+			a.download=fileName;
+		}
+		a.click() // then download it automatically 
+	} 
+	return a
+}
+
+convert2Plotly.gwas=dt=>{
+    
+    // find chromossome and position
+    //let chr=dt.data.map(x=>parseInt(x[0][0].match(/^\d+/)[0]))
+    let chr = dt.data.map(x=>x[0][8]) // chr.iCOGs
+    //let pos=dt.data.map(x=>parseInt(x[0][0].match(/_\d+_/)[0].slice(1,-1)))
+    let pos = dt.data.map(x=>x[0][9]) // Position.iCOGs
+    let chrs = [... new Set(chr)]
+    //let p =dt.data.map(x=>x[0][38]) // P1df_risk_LRT.Onco
+    let p =dt.data.map(x=>(x[0][45]||0.000001))  // p.meta
+    let pLog=p.map(x=>-Math.log10(x))
+    debugger
+    // try with a single chromossome
+    chrs.forEach(chri=>{
+    	console.log(`ploting chr ${chri}`)
+        let trace={
+            x:[],
+            y:[],
+            mode: 'markers',
+            marker: { size: 3 }
+        }
+        chr.forEach((chrj,j)=>{
+            if(chrj==chri){
+                trace.x.push(pos[j])
+                trace.y.push(pLog[j])
+            }
+        })
+        let obj = {
+			traces:[trace],
+			layout:{
+				title:`Chromossome ${chri}`,
+				xaxis:{title:'position'},
+				yaxis:{title:'-log<sub>10</sub>(p)'}
+			},
+		}
+		convert2Plotly.saveFile(JSON.stringify(obj),`chromossome${chri}.json`)
+			
+        debugger
+    })  
+}
+
+//convert2Plotly("GWAS_data374674.json")
--- plco.js
+++ plco.js
@@ -36,6 +36,48 @@ plco.plotTest=async(chr=1,div,url='https://exploregwas-dev.cancer.gov/plco-atlas
    return div
 }
 
+plco.api={}
+
+plco.api.url='https://exploregwas-dev.cancer.gov/plco-atlas/api/'
+
+plco.api.ping=async()=>{
+    return (await fetch(plco.api.url+'ping')).text()
+}
+
+plco.api.get=async(cmd="ping",parms={})=>{
+    if(cmd=="ping"){
+        return await(await fetch(plco.api.url+'ping')).text()=="true"? true : false;
+    }else{
+        return (await fetch(plco.api.url+cmd+'?'+plco.api.parms2string(parms))).json()
+    }
+}
+
+plco.api.string2parms=(str="phenotype_id=3080&sex=all&ancestry=east_asian&p_value_nlog_min=4")=>{
+    let prm={}
+    str.split('&').forEach(s=>{
+        s=s.split('=')
+        prm[s[0]]=s[1]
+    })
+    return prm
+}
+
+plco.api.parms2string=(prm={phenotype_id:3080,sex:"all",ancestry:"east_asian",p_value_nlog_min:4})=>{
+    return Object.keys(prm).map(p=>`${p}=${prm[p]}`).join('&')
+}
+
+plco.api.summary=async(parms)=>{
+    parms= typeof(parms)=="string" ? plco.api.string2parms(parms) : parms
+    parms=parms|{
+        phenotype_id:3080,
+        sex:"all",
+        ancestry:"east_asian",
+        p_value_nlog_min:4
+    }
+    return await plco.api.get(cmd="summary",parms)
+    //phenotype_id=3080&sex=all&ancestry=east_asian&p_value_nlog_min=4"
+}
+
+/*
 if(location.href.match('localhost')||location.href.match('127.0.0.1')){
     let scriptHost=location.href.replace(/\/[^\/]*$/,'/')
     plco.loadScript(`${scriptHost}plcoJonas.js`)
@@ -44,7 +86,25 @@ if(location.href.match('localhost')||location.href.match('127.0.0.1')){
     plco.loadScript('https://episphere.github.io/plco/plcoJonas.js')
     plco.loadScript('https://episphere.github.io/plco/plcoLorena.js')
 }
+*/
 
 if(typeof(define)!='undefined'){
     define(plco)
 }
+
+/*
+if(typeof(define)!='undefined'){
+
+    define([
+    'https://cdn.plot.ly/plotly-latest.min.js',
+    //'https://episphere.github.io/plco/plco',
+    //'https://episphere.github.io/plco/plcoJonas.js',
+    //'https://episphere.github.io/plco/plcoLorena.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/localforage/1.9.0/localforage.min.js'
+    ],function(Plotly,localforage){
+        plco.localforage=localforage
+        plco.Plotly=Plotly
+        return plco
+    })
+}
+*/
