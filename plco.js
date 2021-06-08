c4a06f75baf155969acd75b1464a4a7de6ec576d
Eric Ruan <eric.ruan.130@gmail.com>
2021-06-08 16:27:45 -0400

    fixed spacing

--- plco.js
+++ plco.js
@@ -1,8 +1,7 @@
 console.log('plco.js loaded')
 
-plco={
-    date:new Date()
-
+plco = {
+    date: new Date()
 }
 
 /**
@@ -13,13 +12,13 @@ plco={
  * Modified from https://github.com/jonasalmeida/jmat.
  * @param {string} url The download link.
  * @returns {HTMLAnchorElement} HTMLAnchorElement.
- */ 
+ */
 plco.saveFile = (url) => {
     url = url || 'https://downloadgwas-dev.cancer.gov/j_breast_cancer.tsv.gz'
     let a = document.createElement('a')
     a.href = url
     a.target = '_blank'
-    a.click() 
+    a.click()
     return a
 }
 
@@ -38,53 +37,53 @@ plco.defineAttributes = (obj, m_fields, o_fields = {}) => {
         }
     })
 }
- 
 
-plco.loadScript=async(url,host)=>{
+
+plco.loadScript = async (url, host) => {
     let s = document.createElement('script')
-    s.src=url
+    s.src = url
     return document.head.appendChild(s)
 }
 
 plco.loadScript("https://cdn.plot.ly/plotly-latest.min.js")
 
-plco.plotTest=async(chr=1,div,url='https://exploregwas-dev.cancer.gov/plco-atlas/api/summary?phenotype_id=3080&sex=female&ancestry=east_asian&p_value_nlog_min=2&raw=true')=>{
-   let xx = await (await fetch(url)).json()
-   div=div||document.createElement('div')
-   let dt = xx.data.filter(x=>x[4]==chr)
-   trace={
-       x:dt.map(d=>d[5]),
-       y:dt.map(d=>d[6]),
-       mode: 'markers',
-       type: 'scatter'
-   }
-   let layout={
-       title:`Chromosoome ${chr}`,
-       xaxis:{
-           title:'position'
-       },
-       yaxis:{
-           title:'-log(p)'
-       }
-   }
-   Plotly.newPlot(div,[trace],layout)
-   return div
+plco.plotTest = async (chr = 1, div, url = 'https://exploregwas-dev.cancer.gov/plco-atlas/api/summary?phenotype_id=3080&sex=female&ancestry=east_asian&p_value_nlog_min=2&raw=true') => {
+    let xx = await (await fetch(url)).json()
+    div = div || document.createElement('div')
+    let dt = xx.data.filter(x => x[4] == chr)
+    trace = {
+        x: dt.map(d => d[5]),
+        y: dt.map(d => d[6]),
+        mode: 'markers',
+        type: 'scatter'
+    }
+    let layout = {
+        title: `Chromosoome ${chr}`,
+        xaxis: {
+            title: 'position'
+        },
+        yaxis: {
+            title: '-log(p)'
+        }
+    }
+    Plotly.newPlot(div, [trace], layout)
+    return div
 }
 
-plco.api={}
+plco.api = {}
 
-plco.api.url='https://exploregwas-dev.cancer.gov/plco-atlas/api/'
+plco.api.url = 'https://exploregwas-dev.cancer.gov/plco-atlas/api/'
 
-plco.api.ping=async()=>{
-    return (await fetch(plco.api.url+'ping')).text()
+plco.api.ping = async () => {
+    return (await fetch(plco.api.url + 'ping')).text()
 }
 
-plco.api.get=async(cmd="ping",parms={})=>{
+plco.api.get = async (cmd = "ping", parms = {}) => {
     // res = await fetch(...)
     // content-type = await res.blob().type
-    if(cmd=="ping"){
-        return await(await fetch(plco.api.url+'ping')).text()=="true"? true : false;
-    } else if (cmd==='download') {
+    if (cmd == "ping") {
+        return await (await fetch(plco.api.url + 'ping')).text() == "true" ? true : false
+    } else if (cmd === 'download') {
         if (parms['get_link_only'] === 'true') {
             return (
                 await fetch(
@@ -97,22 +96,26 @@ plco.api.get=async(cmd="ping",parms={})=>{
             )
             return await new Promise((resolve) => resolve({}))
         }
-    }else{
-        return (await fetch(plco.api.url+cmd+'?'+plco.api.parms2string(parms))).json()
+    } else {
+        return (await fetch(plco.api.url + cmd + '?' + plco.api.parms2string(parms))).json()
     }
 }
 
-plco.api.string2parms=(str="phenotype_id=3080&sex=all&ancestry=east_asian&p_value_nlog_min=4")=>{
-    let prm={}
-    str.split('&').forEach(s=>{
-        s=s.split('=')
-        prm[s[0]]=s[1]
+plco.api.string2parms = (
+    str = "phenotype_id=3080&sex=all&ancestry=east_asian&p_value_nlog_min=4"
+) => {
+    let prm = {}
+    str.split('&').forEach(s => {
+        s = s.split('=')
+        prm[s[0]] = s[1]
     })
     return prm
 }
 
-plco.api.parms2string=(prm={phenotype_id:3080,sex:"all",ancestry:"east_asian",p_value_nlog_min:4})=>{
-    return Object.keys(prm).map(p=>`${p}=${prm[p]}`).join('&')
+plco.api.parms2string = (
+    prm = { phenotype_id: 3080, sex: "all", ancestry: "east_asian", p_value_nlog_min: 4 }
+) => {
+    return Object.keys(prm).map(p => `${p}=${prm[p]}`).join('&')
 }
 
 plco.api.download = async (
@@ -124,8 +127,8 @@ plco.api.download = async (
         typeof parms == 'string'
             ? plco.api.string2parms(parms)
             : Array.isArray(parms)
-            ? Object.fromEntries(parms)
-            : parms
+                ? Object.fromEntries(parms)
+                : parms
     parms = parms || {
         phenotype_id,
     }
@@ -144,8 +147,8 @@ plco.api.participants = async (
         typeof parms == 'string'
             ? plco.api.string2parms(parms)
             : Array.isArray(parms)
-            ? Object.fromEntries(parms)
-            : parms
+                ? Object.fromEntries(parms)
+                : parms
     parms = parms || {
         phenotype_id,
         columns: 'value',
@@ -155,15 +158,15 @@ plco.api.participants = async (
     return await plco.api.get((cmd = 'participants'), parms)
 }
 
-plco.api.summary=async(parms)=>{
-    parms= typeof(parms)=="string" ? plco.api.string2parms(parms) : parms
-    parms=parms||{
-        phenotype_id:3080,
-        sex:"all",
-        ancestry:"east_asian",
-        p_value_nlog_min:4
+plco.api.summary = async (parms) => {
+    parms = typeof (parms) == "string" ? plco.api.string2parms(parms) : parms
+    parms = parms || {
+        phenotype_id: 3080,
+        sex: "all",
+        ancestry: "east_asian",
+        p_value_nlog_min: 4
     }
-    return await plco.api.get(cmd="summary",parms)
+    return await plco.api.get(cmd = "summary", parms)
     //phenotype_id=3080&sex=all&ancestry=east_asian&p_value_nlog_min=4"
 }
 
@@ -178,7 +181,7 @@ if(location.href.match('localhost')||location.href.match('127.0.0.1')){
 }
 */
 
-if(typeof(define)!='undefined'){
+if (typeof (define) != 'undefined') {
     define(plco)
 }
 
