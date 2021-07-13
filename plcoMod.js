ae1b31f37ff265889acf06eea1f2975cac3f16c9
Jonas Almeida <jalmeida@mathbiol.org>
2021-07-13 00:01:21 -0400

    satisfying Plotly dependency

---
+++ plcoMod.js
@@ -0,0 +1,29 @@
+console.log('plcoMod.js loaded')
+
+plcoMod={
+    created:Date()
+}
+
+plcoMod.loadScript = async (url) => {
+    let s = document.createElement('script')
+    s.src = url
+    return document.head.appendChild(s)
+}
+
+if(typeof(define)!="undefined"){
+    //define(plcoMod)
+    define(['https://cdn.plot.ly/plotly-latest.min.js'],function(Plotly){
+        plcoMod.Plotly=Plotly
+        return plcoMod
+    })
+}else{
+    // satisfy Plotly dependency already
+    let s = plcoMod.loadScript('https://cdn.plot.ly/plotly-latest.min.js')
+    s.then(async s => {
+        s.onload=()=>{
+            plcoMod.Plotly=Plotly
+        }
+    })
+}
+
+console.log(plcoMod)
